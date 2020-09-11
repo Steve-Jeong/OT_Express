041b8bcd303b1e796fb12e7e120838e1517a4360
Steve Jeong <jst0930@gmail.com>
2020-09-12 08:52:35 +0900

    applying middleware

--- main.js
+++ main.js
@@ -5,123 +5,105 @@ const template = require('./lib/template.js');
 const qs = require('querystring');
 const path = require('path');
 const sanitizeHtml = require('sanitize-html');
+const bodyParser = require('body-parser');
+const compression = require('compression')
+
+app.use(compression({level:6}))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.get('*', (request, response, next) => {
+  fs.readdir('data', 'utf8', (err, filelist) => {
+    request.filelist = filelist;
+    next();
+  })
+})
+
 
 app.get('/', (request, response) => {
   let title = 'Welcome';
   let description = 'Hello NodeJS';
-  fs.readdir('data', 'utf8', (err, filelist) => {
-    let contents = template.Contents(filelist);
-    template.makePage(title, contents, template.Body(title, description), `<a href="/create">create</a>`, response)
-  })
+  let contents = template.Contents(request.filelist);
+  template.makePage(title, contents, template.Body(title, description), `<a href="/create">create</a>`, response)
 })
 
 app.get('/page/:id', (request, response) => {
   var title = request.params.id;
   var filteredTitle = path.parse(title).base;
-  console.log(request.params);
-  fs.readdir('data', 'utf8', (err, filelist) => {
-    fs.readFile(`data/${filteredTitle}`, 'utf8', (err, description) => {
-      var contents = template.Contents(filelist);
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description);
-      template.makePage(sanitizedTitle, contents, template.Body(sanitizedTitle, sanitizedDescription),
-        `<a href="/create">create</a> 
-        <a href="/update/${sanitizedTitle}">update</a>
-        <form action="/delete_process" method="post">
-          <input type="hidden" name="id" value="${sanitizedTitle}">
-          <input type="submit" value="delete">
-        </form>
-        `, response)
-    })
+  fs.readFile(`data/${filteredTitle}`, 'utf8', (err, description) => {
+    var contents = template.Contents(request.filelist);
+    var sanitizedTitle = sanitizeHtml(title);
+    var sanitizedDescription = sanitizeHtml(description);
+    template.makePage(sanitizedTitle, contents, template.Body(sanitizedTitle, sanitizedDescription),
+      `<a href="/create">create</a> 
+      <a href="/update/${sanitizedTitle}">update</a>
+      <form action="/delete_process" method="post">
+        <input type="hidden" name="id" value="${sanitizedTitle}">
+        <input type="submit" value="delete">
+      </form>
+      `, response)
   })
 })
 
 app.get('/create', (request, response) => {
   title = 'Welcome';
   description = 'Hello NodeJS';
-  fs.readdir('data', 'utf8', (err, filelist) => {
-    var contents = template.Contents(filelist);
-    template.makePage(title, contents, template.Create(), '', response)
-  })
+  var contents = template.Contents(request.filelist);
+  template.makePage(title, contents, template.Create(), '', response)
 })
 
 app.post('/create_process', (request, response) => {
-  var body = '';
-  request.on('data', function (data) {
-    body += data;
-  })
-
-  request.on('end', function () {
-    var post = qs.parse(body);
-    var title = post.title;
-    var description = post.description;
-    var filteredTitle = path.parse(title).base;
-    fs.writeFile(`data/${filteredTitle}`, description, 'utf8', function (err) {
-      response.redirect(`/page/${title}`);
-    })
+  var post = request.body;
+  var title = post.title;
+  var description = post.description;
+  var filteredTitle = path.parse(title).base;
+  fs.writeFile(`data/${filteredTitle}`, description, 'utf8', function (err) {
+    response.redirect(`/page/${title}`);
   })
 })
 
 app.get('/update/:id', (request, response) => {
   var title = request.params.id;
   var filteredTitle = path.parse(title).base;
-  fs.readdir('data', 'utf8', (err, filelist) => {
-    fs.readFile(`data/${filteredTitle}`, 'utf8', (err, description) => {
-      var contents = template.Contents(filelist);
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description);
-      template.makePage(title, contents, `
-          <form action="http://localhost:3000/update_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <p><input type="text" name='title' placeholder="title" value=${sanitizedTitle}></p>
-            <p><textarea name="description" id="" cols="30" rows="5"  \
-              placeholder="Description">${sanitizedDescription}</textarea></p>
-            <p><input type="submit" value="send"></p>
-          </form>
-        `,
-        ``,
-        response)
-    })
+  fs.readFile(`data/${filteredTitle}`, 'utf8', (err, description) => {
+    var contents = template.Contents(request.filelist);
+    var sanitizedTitle = sanitizeHtml(title);
+    var sanitizedDescription = sanitizeHtml(description);
+    template.makePage(title, contents, `
+        <form action="http://localhost:3000/update_process" method="post">
+          <input type="hidden" name="id" value="${sanitizedTitle}">
+          <p><input type="text" name='title' placeholder="title" value=${sanitizedTitle}></p>
+          <p><textarea name="description" id="" cols="30" rows="5"  \
+            placeholder="Description">${sanitizedDescription}</textarea></p>
+          <p><input type="submit" value="send"></p>
+        </form>
+      `,
+      ``,
+      response)
   })
 })
 
 app.post('/update_process', (request, response) => {
-  var body = '';
-  request.on('data', function (data) {
-    body += data;
-  })
-
-  request.on('end', function () {
-    var post = qs.parse(body);
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    var filteredTitle = path.parse(title).base;
-    fs.rename(`data/${id}`, `data/${filteredTitle}`, function (error) {
-      fs.writeFile(`data/${filteredTitle}`, description, 'utf8', function (err) {
-        response.redirect(`/page/${title}`);
-      })
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  var filteredTitle = path.parse(title).base;
+  fs.rename(`data/${id}`, `data/${filteredTitle}`, function (error) {
+    fs.writeFile(`data/${filteredTitle}`, description, 'utf8', function (err) {
+      response.redirect(`/page/${title}`);
     })
   })
 })
 
 app.post('/delete_process', (request, response) => {
-  var body = '';
-    request.on('data', function (dataflow) {
-      body += dataflow;
-    })
-
-    request.on('end', function () {
-      var post = qs.parse(body);
-      var fn = post.id;
-      var filteredTitle = path.parse(fn).base;
-      fs.unlink(`data/${filteredTitle}`, (err) => {
-        response.redirect(`/`);
-      })
-    })
+  var post = request.body;
+  var fn = post.id;
+  var filteredTitle = path.parse(fn).base;
+  fs.unlink(`data/${filteredTitle}`, (err) => {
+    response.redirect(`/`);
+  })
 })
 
-app.get('*', (req,res) => {
+app.get('*', (req, res) => {
   res.status(404).send('Sorry Invalid Page')
 })
 
@@ -160,7 +142,7 @@ var app = http.createServer(function (request, response) {
 
 
   } else if (pathname === '/delete_process') {
-    
+
   } else {
     response.writeHead(404);
     response.end('<h1><a href="/">WEB</a></h1><p>Page Not Found.</p>');
